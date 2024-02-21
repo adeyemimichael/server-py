b319a2bacf48264fead745430494ee6730f07b33
adeyemimike123 <ayobami732000@gmail.com>
2024-02-21 16:02:02 +0100

    commit later to the code

--- client.py
+++ client.py
@@ -1,31 +1,51 @@
-import socket
-import ipywidgets as widgets
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-PORT = 8080
-MAXLINE = 1024
+#define PORT 8080
+#define MAXLINE 1024
 
-# Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
+    struct sockaddr_in servaddr;
 
-# Filling server information
-servaddr = ('127.0.0.1', PORT)  # Assuming the server is running locally
+    // Create socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-# Define text input widget
-text_input = widgets.Text(placeholder="Enter message to send to server")
-display(text_input)
+    memset(&servaddr, 0, sizeof(servaddr));
 
-def send_message(sender):
-    message = text_input.value.encode()
-    sock.sendto(message, servaddr)
-    print("Message sent to server.")
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
 
-# Define event handler for text input
-text_input.on_submit(send_message)
+    // Prompt user to enter a message to send to the server
+    printf("Enter message to send to server : ");
+    fgets(buffer, sizeof(buffer), stdin);
 
-# Receive message from server
-while True:
-    data, _ = sock.recvfrom(MAXLINE)
-    print("Server:", data.decode())
+    // Send message to server
+    sendto(sockfd, (const char *)buffer, strlen(buffer),
+        0, (const struct sockaddr *)&servaddr,
+        sizeof(servaddr));
+    printf("Message sent to server.\n");
 
-# Close the socket (this won't be reached in an infinite loop)
-sock.close()
+    // Receive message from server
+    int n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+                     0, (struct sockaddr *)&servaddr,
+                     &(socklen_t){sizeof(servaddr)});
+    buffer[n] = '\0';
+    printf("Server: %s\n", buffer);
+
+    // Close the socket
+    close(sockfd);
+
+    return 0;
+}
--- server.py
+++ server.py
@@ -1,26 +1,57 @@
-import socket
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-PORT = 8080
-BUFFER_SIZE = 1024
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
-# Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+int main() {
+    int sockfd;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in servaddr, cliaddr;
+    socklen_t addrlen = sizeof(cliaddr);
 
-# Bind the socket to the address
-sock.bind(('0.0.0.0', PORT))
+    // Create socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-print("UDP server is running...")
+    // Set up server address structure
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
 
-# Receive data from client
-while True:
-    try:
-        data, addr = sock.recvfrom(BUFFER_SIZE)
-        print(f"Received from {addr[0]}:{addr[1]}: {data.decode()}")
+    // Bind the socket to the address
+    if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
 
-        # Send a response (optional)
-        # sock.sendto(b"Hello from server!", addr)
-    except BlockingIOError:
-        pass
+    printf("UDP server is running...\n");
 
-# Close the socket
-sock.close()
+    // Receive data from client
+    while (1) {
+        ssize_t recv_len;
+        if ((recv_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&cliaddr, &addrlen)) == -1) {
+            perror("recvfrom failed");
+            exit(EXIT_FAILURE);
+        }
+        buffer[recv_len] = '\0'; // Null-terminate the received data
+
+        // Print received data and client address
+        printf("Received from %s:%d: %s\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port), buffer);
+
+        // Send a response (optional)
+        // sendto(sockfd, "Hello from server!", strlen("Hello from server!"), 0, (struct sockaddr *)&cliaddr, addrlen);
+    }
+
+    // Close the socket (won't be reached in an infinite loop)
+    close(sockfd);
+
+    return 0;
+}
